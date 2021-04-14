5193b058e37785608e7231af9acde9047f71de71
林 <284590533@qq.com>
2021-04-14 18:26:49 +0800

    fix🐛 修复bug

--- src/components/panel-col.vue
+++ src/components/panel-col.vue
@@ -1,96 +1,108 @@
 <template>
-    <div :ref="refName" class="tticar-panel-col" :data-ref='refName' :style="styleObj">
-        <span ref="handle" class="handle handle-col" v-show="isElderBrother"></span>
-        <slot></slot>
-    </div>
+	<div :ref="refName" class="panel-col" :data-ref="refName" :style="styleObj">
+		<span ref="handle" class="handle handle-col" v-show="isElderBrother"></span>
+		<slot></slot>
+	</div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            styleObj:{
-                width:"100%"
-            },
-            isElderBrother:false,
-            el:null,
-            parentEl:null,
-            previousEl: null,
-            width: null,
-            parentELWidth:null,
-            previousElWidth:null,
-            startX:null,
-            refName: null
-        }
-    },
-    props: {
-        span:{
-            type: Number,
-            default: 24
-        }
-    },
-    computed: {
-        gutter() {
-            let parent = this.$parent;
-            return parent ? parent.gutter : 0;
-        },
-    },
-    methods: {
-        getWidth(el){
-            let width = window.getComputedStyle(el).width
-            return parseFloat(width.substring(0,width.length-2));
-        },
-        onMouseDown (e) {
-            let _this = this;
-            return function(e){
-                _this.startX = e.x
-                _this.width = _this.getWidth(_this.el);
-                _this.parentELWidth = _this.getWidth(_this.parentEl);
-                _this.previousElWidth = _this.getWidth(_this.previousEl);
-                document.addEventListener('mousemove',_this.onMouseMove(event))
-                document.addEventListener('mouseup',_this.onMouseUp(event))
-            }
-        },
-        onMouseMove (e) {
-            return this.mouseMoveHandle
-        },
-        onMouseUp (e) {
-            return this.mouseUpHandle
-        },
-        mouseMoveHandle(e){
-            let distance = this.startX-e.x;
-            let percentWidth = (this.width+distance)/this.parentELWidth*100+'%'
-            let percentpreviousElWidth = (this.previousElWidth-distance)/this.parentELWidth*100+'%'
-            this.styleObj.width = percentWidth;
-            this.previousEl.style.width = percentpreviousElWidth;
-        },
-        mouseUpHandle(e){
-            let _this = this;
-            document.removeEventListener('mousemove',this.mouseMoveHandle)
-            document.removeEventListener('mouseup',this.mouseUpHandle)
-        },
-    },
-    created() {
-        this.refName = 'panelCol_'+this._uid;
-    },
-    mounted() {
-        let _this = this;
-        this.el = this.$refs[this.refName];
-        this.styleObj.width = this.span/24*100+'%'
-        this.parentEl = this.el.parentNode;
-        this.previousEl = this.el.previousElementSibling;
-        if(this.previousEl){
-            this.isElderBrother = true;
-        }
-        this.$refs.handle.addEventListener('mousedown', _this.onMouseDown(event));
-        if (this.gutter) {
-            this.styleObj.paddingLeft = this.styleObj.paddingRight = this.gutter / 2 + 'px';
-        }
+	data() {
+		return {
+			styleObj: {
+				width: '100%',
+			},
+			isElderBrother: false,
+			el: null,
+			parentEl: null,
+			previousEl: null,
+			width: null,
+			parentELWidth: null,
+			previousElWidth: null,
+			startX: null,
+			refName: null,
+		}
+	},
+	props: {
+		span: {
+			type: Number,
+			default: 24,
+		},
+	},
+	computed: {
+		gutter() {
+			let parent = this.$parent
+			return parent ? parent.gutter : 0
+		},
+	},
+	methods: {
+		getWidth(el) {
+			let width = window.getComputedStyle(el).width
+			return parseFloat(width.substring(0, width.length - 2))
+		},
+		onMouseDown(e) {
+			return e => {
+        document.body.style.userSelect = 'none'
+				this.startX = e.x
+				this.width = this.getWidth(this.el)
+				this.parentELWidth = this.getWidth(this.parentEl)
+				this.previousElWidth = this.getWidth(this.previousEl)
+				document.addEventListener('mousemove', this.onMouseMove())
+				document.addEventListener('mouseup', this.onMouseUp())
+			}
+		},
+		onMouseMove(e) {
+			return this.mouseMoveHandle
+		},
+		onMouseUp(e) {
+			return this.mouseUpHandle
+		},
+		mouseMoveHandle(e) {
+			let distance = this.startX - e.x
+      let diffWidth = this.width + distance
+      let previousDiffWidth = this.previousElWidth - distance
+      if(diffWidth>=this.previousElWidth + this.width) {
+        diffWidth = this.previousElWidth + this.width
+        previousDiffWidth = 0
+      }
 
-    }
+      if(previousDiffWidth >=this.previousElWidth + this.width) {
+        previousDiffWidth = this.width + this.previousElWidth
+        diffWidth = 0
+      }
 
+      if(diffWidth <= 0) {
+        this.previousDiffWidth = this.previousElWidth + this.width
+        diffWidth = 0
+      }
+
+			let percentWidth = (diffWidth / this.parentELWidth) * 100 + '%'
+			let percentpreviousElWidth = (previousDiffWidth / this.parentELWidth) * 100 + '%'
+			this.styleObj.width = percentWidth
+			this.previousEl.style.width = percentpreviousElWidth
+		},
+		mouseUpHandle(e) {
+      document.body.style.userSelect = 'initial'
+			document.removeEventListener('mousemove', this.mouseMoveHandle)
+			document.removeEventListener('mouseup', this.mouseUpHandle)
+		},
+	},
+	created() {
+		this.refName = 'panelCol_' + this._uid
+	},
+	mounted() {
+		let _this = this
+		this.el = this.$refs[this.refName]
+		this.styleObj.width = (this.span / 24) * 100 + '%'
+		this.parentEl = this.el.parentNode
+		this.previousEl = this.el.previousElementSibling
+		if (this.previousEl) {
+			this.isElderBrother = true
+		}
+		this.$refs.handle.addEventListener('mousedown', _this.onMouseDown())
+		if (this.gutter) {
+			this.styleObj.paddingLeft = this.styleObj.paddingRight = this.gutter / 2 + 'px'
+		}
+	},
 }
 </script>
-
-
-
--- src/components/panel-row.vue
+++ src/components/panel-row.vue
@@ -1,136 +1,145 @@
 <template>
-    <div :ref="refName" class="tticar-panel-row" :data-ref='refName' :style="styleObj">
-        <slot></slot>
-        <span ref="handle" class="handle handle-row" v-show="isElderBrother" :style="{left:gutter/2+'px',right:gutter/2+'px'}"></span>
-    </div>
+	<div :ref="refName" class="panel-row" :data-ref="refName" :style="styleObj">
+		<slot></slot>
+		<span ref="handle" class="handle handle-row" v-show="isElderBrother" :style="style"></span>
+	</div>
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-            styleObj:{
-                height:"100%"
-            },
-            isElderBrother:false,
-            el:null,
-            parentEl:null,
-            previousEl: null,
-            _height: null,
-            parentELHeight:null,
-            previousElHeight:null,
-            startY:null,
-            refName:null,
-            handleClass:''
-        }
-    },
-    props: {
-        // 高度百分比
-        height:{
-            type: Number,
-            default: 100
-        },
+	data() {
+		return {
+			styleObj: {
+				height: '100%',
+			},
+			isElderBrother: false,
+			el: null,
+			parentEl: null,
+			previousEl: null,
+			_height: null,
+			parentELHeight: null,
+			previousElHeight: null,
+			startY: null,
+			refName: null,
+			handleClass: '',
+		}
+	},
+	props: {
+		// 高度百分比
+		height: {
+			type: Number,
+			default: 100,
+		},
 
-        // 单元格panel-col间距
-        gutter:{
-            type: Number,
-            default: 0
-        },
+		// 单元格panel-col间距
+		gutter: {
+			type: Number,
+			default: 0,
+		},
 
-        // 子元素高度
-        value: {
-            type: Number,
-        }
-    },
-    methods: {
-        getHeight(el){
-            let height = window.getComputedStyle(el).height;
-            return parseFloat(height.substring(0,height.length-2));
-        },
-        onMouseDown (e) {
-            let _this = this;
-            return function(e){
-                _this.startY= e.y
-                _this._height = _this.getHeight(_this.el);
-                _this.parentELHeight = _this.getHeight(_this.parentEl);
-                _this.previousElHeight = _this.getHeight(_this.previousEl);
-                document.addEventListener('mousemove',_this.onMouseMove(event))
-                document.addEventListener('mouseup',_this.onMouseUp(event))
-            }
-        },
-        onMouseMove (e) {
-            return this.mouseMoveHandle
-        },
-        onMouseUp (e) {
-            return this.mouseUpHandle
-        },
-        mouseMoveHandle(e){
-            let distance = this.startY-e.y;
-            let height = this._height+distance;
-            let previousElHeight = this.previousElHeight-distance;
-            let percentHeight= height/this.parentELHeight*100+'%'
-            let percentpreviousElHeight = previousElHeight/this.parentELHeight*100+'%'
-            this.styleObj.height = percentHeight;
-            this.previousEl.style.height = percentpreviousElHeight;
-            this.emitFun()
-            // 触发实践修改高度，使用 value.sync 传递参数
-            if(this.previousEl){
-                height = height-10;
-            }
-            this.$emit('update:value',height)
+		// 子元素高度
+		value: {
+			type: Number,
+		},
+	},
+	computed: {
+		style() {
+			return { 
+				left: this.gutter / 2 + 'px', 
+				right: this.gutter / 2 + 'px' 
+			}
+		}
+	},
+	methods: {
+		getHeight(el) {
+			let height = window.getComputedStyle(el).height
+			return parseFloat(height.substring(0, height.length - 2))
+		},
+		onMouseDown(e) {
+			return (e) => {
+        document.body.style.userSelect = 'none'
+				this.startY = e.y
+				this._height = this.getHeight(this.el)
+				this.parentELHeight = this.getHeight(this.parentEl)
+				this.previousElHeight = this.getHeight(this.previousEl)
+				document.addEventListener('mousemove', this.onMouseMove())
+				document.addEventListener('mouseup', this.onMouseUp())
+			}
+		},
+		onMouseMove() {
+			return this.mouseMoveHandle
+		},
+		onMouseUp() {
+			return this.mouseUpHandle
+		},
+		mouseMoveHandle(e) {
+			let distance = this.startY - e.y
+			let height = this._height + distance
+			let previousElHeight = this.previousElHeight - distance
+			if(height >= this.parentELHeight) {
+				height = this.parentELHeight
+				previousElHeight = 0
+			}
 
-        },
-        mouseUpHandle(e){
-            let _this = this;
-            document.removeEventListener('mousemove',this.mouseMoveHandle)
-            document.removeEventListener('mouseup',this.mouseUpHandle)
-        },
+			if(previousElHeight >= this.parentELHeight ) {
+				previousElHeight = this.parentELHeight
+				height = 0
+			}
+			let percentHeight = (height / this.parentELHeight) * 100 + '%'
+			let percentpreviousElHeight = (previousElHeight / this.parentELHeight) * 100 + '%'
+			this.styleObj.height = percentHeight
+			this.previousEl.style.height = percentpreviousElHeight
+			this.emitFun()
+			// 触发实践修改高度，使用 value.sync 传递参数
+			if (this.previousEl) {
+				height = height - 10
+			}
+			this.$emit('update:value', height)
+		},
+		mouseUpHandle(e) {
+      document.body.style.userSelect = 'initial'
+			document.removeEventListener('mousemove', this.mouseMoveHandle)
+			document.removeEventListener('mouseup', this.mouseUpHandle)
+		},
 
-        // 传refName给父元素通过父元素找到对应的兄弟元素并执行兄弟元素的eventPublish方法
-        emitFun(){
-            let refName = this.previousEl.getAttribute('data-ref');
-            this.$parent.rowResize(refName)
-        },
+		// 传refName给父元素通过父元素找到对应的兄弟元素并执行兄弟元素的eventPublish方法
+		emitFun() {
+			let refName = this.previousEl.getAttribute('data-ref')
+			this.$parent.rowResize(refName)
+		},
 
-        // 触发数据更新事件方法
-        eventPublish(){
-            let height = this.getHeight(this.el);
-            if(this.previousEl){
-                height = height-10;
-            }
-            this.$emit('update:value',height)
-        }
-    },
-    created() {
-        let _this = this;
-        this.refName = 'panelRow_'+this._uid;
-    },
-    mounted() {
-        let _this = this;
-        this.ref = 'panelRow_'+this._uid;
-        this.el = this.$refs[this.refName];
-        this.styleObj.height = this.height+'%'
-        this.parentEl = this.el.parentNode;
-        this.previousEl = this.el.previousElementSibling;
-        console.log(this.previousEl)
-        if(this.previousEl){
-            this.isElderBrother = true;
-            this.styleObj.paddingTop = '10px'
-            this.$nextTick(() => {
-                this.eventPublish();
-                this.emitFun();
-            })
-        }
+		// 触发数据更新事件方法
+		eventPublish() {
+			let height = this.getHeight(this.el)
+			if (this.previousEl) {
+				height = height - 10
+			}
+			this.$emit('update:value', height)
+		},
+	},
+	created() {
+		this.refName = 'panelRow_' + this._uid
+	},
+	mounted() {
+		this.ref = 'panelRow_' + this._uid
+		this.el = this.$refs[this.refName]
+		this.styleObj.height = this.height + '%'
+		this.parentEl = this.el.parentNode
+		this.previousEl = this.el.previousElementSibling
+		if (this.previousEl) {
+			this.isElderBrother = true
+			// this.styleObj.paddingTop = '10px'
+			this.$nextTick(() => {
+				this.eventPublish()
+				this.emitFun()
+			})
+		}
 
-        if (this.gutter) {
-            let handle = this.$refs.handle;
-            this.styleObj.marginLeft = this.styleObj.marginRight = `-${this.gutter / 2}px`;
-        }
-        // 添加鼠标点下事件监听
-        this.$refs.handle.addEventListener('mousedown', _this.onMouseDown(event));
-    },
+		if (this.gutter) {
+			this.styleObj.marginLeft = this.styleObj.marginRight = `-${this.gutter / 2}px`
+		}
+		// 添加鼠标点下事件监听
+		this.$refs.handle.addEventListener('mousedown', this.onMouseDown())
+	},
 }
 </script>
-
-
-
